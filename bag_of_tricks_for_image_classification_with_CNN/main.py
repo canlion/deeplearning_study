f2287999d46d7deba565dc3e7a75a2b25482ce51
canlion <canlion@naver.com>
2020-02-24 01:38:52 +0900

    initial commit - main.py
    네트워크 훈련과 검증.

---
+++ bag_of_tricks_for_image_classification_with_CNN/main.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+from dataset import ImagenetDataset
+from network import Resnet50
+import shutil
+import os
+import time
+
+
+# const.
+BATCH_SIZE = 128
+EPOCHS = 120
+
+
+@tf.function
+def train_network(x, y):
+    with tf.GradientTape() as tape:
+        outp = model(x, training=True)
+        loss = loss_obj(y, outp)
+        loss_l2 = loss + tf.math.add_n(model.losses)
+    gradients = tape.gradient(loss_l2, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss_mean.update_state(loss)
+    train_l2_loss_mean.update_state(loss_l2)
+
+
+@tf.function
+def valid_network(x, y):
+    outp = model(x, training=False)
+    loss = loss_obj(y, outp)
+
+    valid_loss_mean.update_state(loss)
+    top_1_acc.update_state(y, outp)
+    top_5_acc.update_state(y, outp)
+
+
+# dataset
+imagenet_ds = ImagenetDataset('/tf_datasets')
+train_size = imagenet_ds.train_num
+train_ds = imagenet_ds.get_train_ds(BATCH_SIZE)
+valid_ds = imagenet_ds.get_valid_ds(BATCH_SIZE)
+
+# network
+model = Resnet50()
+
+# loss & metric
+loss_obj = tf.keras.losses.CategoricalCrossentropy()
+
+train_loss_mean = tf.keras.metrics.Mean(name='train_loss')
+train_l2_loss_mean = tf.keras.metrics.Mean(name='train_l2_loss')
+valid_loss_mean = tf.keras.metrics.Mean(name='valid_loss')
+top_1_acc = tf.keras.metrics.TopKCategoricalAccuracy(k=1, name='top_1_acc')
+top_5_acc = tf.keras.metrics.TopKCategoricalAccuracy(k=5, name='top_5_acc')
+
+# lr & optimizer
+lr = tf.keras.optimizers.schedules.ExponentialDecay(1e-1, (train_size // BATCH_SIZE) * 30, .1, True)
+optimizer = tf.keras.optimizers.SGD(learning_rate=lr, nesterov=True)
+
+# tensorboard
+log_dir = './logs'
+shutil.rmtree(os.path.join(log_dir, '*'), ignore_errors=True)
+
+train_loss_log_dir = os.path.join(log_dir, 'train_loss')
+train_l2_loss_log_dir = os.path.join(log_dir, 'train_l2_loss')
+valid_loss_log_dir = os.path.join(log_dir, 'valid_loss')
+top_1_log_dir = os.path.join(log_dir, 'top_1_acc')
+top_5_log_dir = os.path.join(log_dir, 'top_5_acc')
+
+train_loss_writer = tf.summary.create_file_writer(train_loss_log_dir)
+train_l2_loss_writer = tf.summary.create_file_writer(train_l2_loss_log_dir)
+valid_loss_writer = tf.summary.create_file_writer(valid_loss_log_dir)
+top_1_writer = tf.summary.create_file_writer(top_1_log_dir)
+top_5_writer = tf.summary.create_file_writer(top_5_log_dir)
+
+report_format = """\
+{} epoch - t_loss : {:.3f} / t_l2_loss : {:.3f} / v_loss : {:.3f}
+           top_1 : {:.3f} / top_5 :{:.3f} - time : {}"""
+
+for epoch in range(EPOCHS):
+    train_loss_mean.reset_states()
+    train_l2_loss_mean.reset_states()
+    valid_loss_mean.reset_states()
+    top_1_acc.reset_states()
+    top_5_acc.reset_states()
+
+    t_start = time.time()
+    for img, label in train_ds:
+        train_network(img, label)
+    with train_loss_writer.as_default():
+        tf.summary.scalar('loss', train_loss_mean.result(), step=epoch)
+    with train_l2_loss_writer.as_default():
+        tf.summary.scalar('loss', train_l2_loss_mean.result(), step=epoch)
+
+    for img, label in valid_ds:
+        valid_network(img, label)
+    with valid_loss_writer.as_default():
+        tf.summary.scalar('loss', valid_loss_mean.result(), step=epoch)
+    with top_1_writer.as_default():
+        tf.summary.scalar('acc', top_1_acc.result(), step=epoch)
+    with top_5_writer.as_default():
+        tf.summary.scalar('acc', top_5_acc.result(), step=epoch)
+    t_end = time.time()
+
+    print(report_format.format(epoch,
+                               train_loss_mean.result(),
+                               train_l2_loss_mean.result(),
+                               valid_loss_mean.result(),
+                               top_1_acc.result(),
+                               top_5_acc.result(),
+                               int(t_end - t_start)))
